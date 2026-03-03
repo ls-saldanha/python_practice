66c3ffc86681696adf37fef92a2d1777adf93519
lucasdev <lucassal16@gmail.com>
2026-03-03 16:22:42 -0300

    Criado projeto aula03, exercicio 1 adicionado

--- aula02/exercicios.py
+++ aula02/exercicios.py
@@ -41,10 +41,20 @@ print(f"O resultado da multiplicação é: {resultado}")
 
 # 4. Faça um programa que peça dois números inteiros 
 # e imprima a divisão inteira do primeiro pelo segundo.
-x = int(input("Forneça um numero inteiro: "))
-y = int(input("Forneça um segundo número inteiro: "))
-z = x // y
-print("O resultado da divisão inteira é: ", z)
+try: 
+    x = int(input("Forneça um numero inteiro: "))
+    y = int(input("Forneça um segundo número inteiro: "))
+    z = x // y
+    print("O resultado da divisão inteira é: ", z)
+
+except ValueError:
+    print("Os números fornecidos tem de ser números inteiros!")
+
+except TypeError:
+    print("Os números fornecidos tem de ser números inteiros!")
+
+except ZeroDivisionError:
+    print("O segundo número não pode ser 0!")
 
 
 #------------------------------ 
@@ -69,7 +79,10 @@ print(f"A temperatura em fahrenheit é: {temp_fah}ºF")
 # 11. Escreva um programa que receba uma string do usuário e a converta para maiúsculas.
 # 12. Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
 # 13. Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
-# 14. Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
+# 14. Faça um programa que peça ao usuário para digitar uma data 
+# no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
+
+
 # 15. Escreva um programa que concatene duas strings fornecidas pelo usuário.
 
 # #### Booleanos (`bool`)
