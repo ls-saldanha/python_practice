b4a78bc379e973f853bed0bfca67b77b57b76f0d
lucasdev <lucassal16@gmail.com>
2026-03-04 17:45:59 -0300

    adicionado exercicio 6 aula 3

--- aula03/exercicios.py
+++ aula03/exercicios.py
@@ -65,7 +65,17 @@ else:
 
 ### Exercício 6. Contagem de Palavras em Textos
 # Objetivo:** Dado um texto, contar quantas vezes cada palavra única aparece nele.
+texto = "a raposa marrom salta sobre o cachorro preguiçoso"
+palavras = texto.split()
+contagem_palavras = {}
 
+for palavra in palavras:
+    if palavra in contagem_palavras:
+        contagem_palavras[palavra] += 1
+    else:
+        contagem_palavras[palavra] = 1
+
+print(contagem_palavras)
 ### Exercício 7. Normalização de Dados
 # Objetivo:** Normalizar uma lista de números para que fiquem na escala de 0 a 1.
 
